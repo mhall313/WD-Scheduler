7c85a884dd176d80c65a5b8c241603bd28c670d0
Melanie Hall <hallmt313@gmail.com>
2020-09-19 15:38:41 -0400

    Time and Date Display and other small styling adjustments

--- assets/javascript.js
+++ assets/javascript.js
@@ -15,6 +15,35 @@ console.log(
     time.toLocaleString('en-US', { hour: 'numeric', hour12: true })
 );  
 
-$("#currentDay").html(time);
+var dt = new Date();
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
 
+var month = new Array(12);
+month[0] = "Jan";
+month[1] = "Feb";
+month[2] = "Mar";
+month[3] = "Apr";
+month[4] = "May";
+month[5] = "Jun";
+month[6] = "Jul";
+month[7] = "Augt";
+month[8] = "Sep";
+month[9] = "Oct";
+month[10] = "Nov";
+month[11] = "Dec";
+
+
+//Saturday Sep 19, 2020
+var d = weekday[dt.getDay()];
+var calD = month[dt.getMonth()] + " " + dt.getDate() + ", " + dt.getFullYear();
+
+$("#currentDay").html(d);
+$("#currentDate").html(calD);
 
